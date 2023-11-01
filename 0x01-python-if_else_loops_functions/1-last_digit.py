2cecd8648752579d4b8829e979da8a9cd71c9588
Eric Ogbekene <ericogbekene@icloud.com>
2023-11-01 14:57:42 -0700

    value return fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,6 +4,8 @@ number = random.randint(-1000, 1000)
 lastDigit = abs(number) % 10
 #print("Last digit of {} is {}".format(number, lastDigit))
 
+if number < 0:
+    lastDigit = -lastDigit
 if lastDigit > 5:
     print(f"Last digit of {number:d} is {lastDigit:d} and is greater than 5")
 elif lastDigit == 0:
