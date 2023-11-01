492c51790bf2d95b430a24df89ad37e596eb9748
Eric Ogbekene <ericogbekene@icloud.com>
2023-11-01 16:33:51 -0700

    output fixe checker

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i in range(0,9):
-        print("{}{}{}{}".format('0', i, ',', ' '), end='')
+        print("{}{}{}".format('0', i, ','), end='')
     else:
         print("{} {}".format(i, ","), end='')
