daccc85b704912b5ec727f5066f62cbab2545e7e
Eric Ogbekene <ericogbekene@icloud.com>
2023-11-02 15:33:01 -0700

    print arguments using syst module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
-count = sys.argv
-len1 = len(count) - 1
-if len1 >= 0:
-    if len1 == 0:
-        print("{:d} arguments .".format(len1))
-    elif len1 == 1:
-        print("{:d} argument".format(len1))
-        print("{:d} : {}".format(len1, count[1]))
-    else:
-        print("{:d} arguments :".format(len1))
-        for i in range (1, (len1 + 1)):
-            print("{:d}: {}".format(i, count[i]))
+if __name__ == '__main__':
+    import sys
+    count = sys.argv
+    len1 = len(count) - 1
+    if len1 >= 0:
+        if len1 == 0:
+            print("{:d} arguments .".format(len1))
+        elif len1 == 1:
+            print("{:d} argument".format(len1))
+            print("{:d} : {}".format(len1, count[1]))
+        else:
+            print("{:d} arguments :".format(len1))
+            for i in range(1, (len1 + 1)):
+                print("{:d}: {}".format(i, count[i]))
