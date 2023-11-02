ec533a23e82555148c8324fadd4cca8fa9d723f8
Eric Ogbekene <ericogbekene@icloud.com>
2023-11-01 17:35:16 -0700

    check last digit

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,8 +2,8 @@
 def fizzbuzz():
     for i in range(1, 100):
         if i % 3 == 0:
-            print("{}".format("Fizz"))
+            print("{}".format("Fizz")end='')
         elif i % 5 == 0:
-            print("{}".format("Buzz"))
+            print("{}".format("Buzz"), end='')
         else:
-            print("{}".format(i))
+            print("{}".format(i), end='')
