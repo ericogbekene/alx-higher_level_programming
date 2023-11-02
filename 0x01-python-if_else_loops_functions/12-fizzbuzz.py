825f9fddeed4c38007807e67126a9dddab0e6ba4
Eric Ogbekene <ericogbekene@icloud.com>
2023-11-01 18:20:55 -0700

    add islower function

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for i in range(1, 100):
         if i % 3 == 0:
-            print("{}".format("Fizz")end='')
+            print("{}".format("Fizz"), end='')
         elif i % 5 == 0:
             print("{}".format("Buzz"), end='')
         else:
