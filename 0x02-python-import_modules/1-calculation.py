4f3a7acdfeb9b72fdd43675b4bf5db21e58a1216
Eric Ogbekene <ericogbekene@icloud.com>
2023-11-02 15:21:31 -0700

    fix import function for stud

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 if __name__ == 'main':
-    from calculator_1 import add, sub, mul, div
+    import calculator_1 as calculus
+
     a = 10
     b = 5
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, calculus.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, calculus.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, calculus.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, calculus.div(a, b)))
