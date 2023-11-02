9af97c9a7e53d75e8175e810d823402050362c58
Eric Ogbekene <ericogbekene@icloud.com>
2023-11-01 17:59:09 -0700

    prints the alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i == ord('q') or i == ord('e'):
-        continue
-    print(f"{chr(i)}", end='')
+    if i != 101 and i != 113:
+        print(f"{chr(i)}", end='')
