a34a63ca57d6cb47c13f1391a9656d5029db2e13
Eric Ogbekene <ericogbekene@icloud.com>
2023-11-04 13:09:50 -0700

    py charm fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    our_list = my_list
     if idx < 0:
-        return my_list
+        return our_list
     if idx > len(my_list):
-        return (my_list)
+        return (our_list)
     else:
         new_list = []
         new_list += my_list
