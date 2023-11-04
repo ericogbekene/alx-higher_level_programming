1fe15920d77f1bec2f470646c2bd0ede70d9bd83
Eric Ogbekene <ericogbekene@icloud.com>
2023-11-04 13:24:30 -0700

    without dot copy method

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    our_list = my_list.copy()
+    our_list = []
+    our_list += my_list
     if idx < 0:
         return our_list
     if idx >= len(my_list):
