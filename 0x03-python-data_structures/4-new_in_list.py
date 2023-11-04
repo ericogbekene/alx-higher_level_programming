908b178b55ac6bbe0752ad6d61bc1817c08701ba
Eric Ogbekene <ericogbekene@icloud.com>
2023-11-04 13:16:16 -0700

    alx take dot copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    our_list = my_list
+    our_list = my_list.copy()
     if idx < 0:
         return our_list
     if idx > len(my_list):
