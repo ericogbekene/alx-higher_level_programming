eb20867aced0c21aea7458d87948d032de3e6357
Eric Ogbekene <ericogbekene@icloud.com>
2023-11-08 14:35:00 -0800

    print common elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    lay = set()
     for idx in set_1:
         if idx in set_2:
-            return idx
+            lay.add(idx)
+            return lay
