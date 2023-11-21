40b4b33b9965b863a006b2a660a17ad4fc573ed6
Eric Ogbekene <ericogbekene@icloud.com>
2023-11-21 12:26:47 -0800

    initiate square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Square:
+    """ class Square """
+
+    def __init__(self, size:int) -> None:
+        """
+        instant of square with size attribute
+        Args:
+            size - size of he square
+        """
+        self.__size = size
