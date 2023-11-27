10d2322ded8831e5f0ebdc43ce47ab26016f5412
Eric Ogbekene <ericogbekene@icloud.com>
2023-11-27 15:12:35 -0800

    function fix for string representation

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -74,4 +74,4 @@ class Rectangle:
 
     def __repr__(self):
         """ to print a string representation of the object """
-        return (f"Rectangle({self.__width}, {self.__height}))
+        return f"Rectangle({self.__width}, {self.__height})"
