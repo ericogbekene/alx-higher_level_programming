2f5276c1eca6c221efd6156bf9f9a2d98cd93d59
Eric Ogbekene <ericogbekene@icloud.com>
2023-12-01 08:17:56 -0800

    Adding text indentation exercise and associated test file

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" function to auto indent """
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for i in text:
+        #print(i)
+        if i == ".":
+            print()
+            print()
+            pass
+        elif i == "?":
+            print()
+            print()
+        elif i == ":":
+            print()
+            print()
+        else:
+            print(i.lstrip(), end='')
