f75e53d69095cf34d9bc69176e2359d4948600e0
Eric Ogbekene <ericogbekene@icloud.com>
2023-12-04 14:10:11 -0800

    checks instance of class or subclass

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" check if object is instance of a class or sublass """
+
+
+def is_kind_of_class(obj, a_class):
+    """ checks instance of a class """
+    return isinstance(obj, a_class)
