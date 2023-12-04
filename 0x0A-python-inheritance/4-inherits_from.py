fcb2fbc16f3b0ec6d498e0955c78ebcab9ad848f
Eric Ogbekene <ericogbekene@icloud.com>
2023-12-04 14:33:48 -0800

    check if an object is a subtype

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" checks if a class is a subtype """
+
+
+def inherits_from(obj, a_class):
+    """ checks if an object is an instance of subclass """
+    return True if issubclass(type(obj), a_class)\
+            and type(obj) != a_class else False
