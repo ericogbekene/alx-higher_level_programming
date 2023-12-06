e70044d015695b04d62d52a2f55118fabc2b020f
Eric Ogbekene <ericogbekene@icloud.com>
2023-12-06 13:30:35 -0800

    add item script

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" script to load add and save """
+
+
+from sys import argv
+import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+===================================================================
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+===================================================================
+
+try:
+    my_items = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    my_items = []
+my_items.extend(argv[1:])
+save_to_json_file(my_items, "add_item.json")
