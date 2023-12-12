a04edce487a9232a4f7c20b8b2dc2df6813b62ec
Eric Ogbekene <ericogbekene@icloud.com>
2023-12-11 16:23:44 -0800

    tests: adding more tests for methods and attributes

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -11,12 +11,27 @@ class Test_Rectangle(unittest.TestCase):
     class to test rectangle
 
     """
+
+    def test_attributes(self):
+        """ testing attributes """
+        r = Rectangle(5, 10)
+        self.assertIsInstance(r.width, int)
+        self.assertIsInstance(r.height, int)
+        self.assertIsInstance(r.x, int)
+        self.assertIsInstance(r.y, int)
+        self.assertIsInstance(r.id, int)
+        self.assertEqual(r.width, 5)
+        self.assertEqual(r.height, 10)
+        self.assertEqual(r.x, 0)
+        self.assertEqual(r.y, 0)
+        self.assertIsNotNone(r.id)
+
     def testing_rectangle(self):
         r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 1)
+        self.assertEqual(r1.id, 5)
 
         r2 = Rectangle(2, 10)
-        self.assertEqual(r2.id, 2)
+        self.assertEqual(r2.id, 6)
 
         r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r3.id, 12)
@@ -27,3 +42,41 @@ class Test_Rectangle(unittest.TestCase):
         r4 = Rectangle("a", 10)
         self.assertRaises(ValueError, r4.id)
         """
+
+    def test_invalid_width(self):
+        """Test invalid width value """
+        with self.assertRaises(ValueError):
+            Rectangle(-5, 10)
+
+    def test_invalid_height(self):
+        """ Test invalid width value """
+        with self.assertRaises(ValueError):
+            Rectangle(5, -10)
+
+    def test_str_method(self):
+        """ Test the __str__ method """
+        r = Rectangle(5, 10, 2, 3, 1)
+        expected_output = "[Rectangle] (1) 2/3 - 5/10"
+        self.assertEqual(str(r), expected_output)
+
+    def test_area_method(self):
+        """ Test the area method """
+        r = Rectangle(5, 10)
+        self.assertEqual(r.area(), 50)
+
+    def test_update_method(self):
+        """ Test the update method """
+        r = Rectangle(5, 10, 2, 3, 1)
+        r.update(2, 8, 12, 4, 6)
+        self.assertEqual(r.width, 8)
+        self.assertEqual(r.height, 12)
+        self.assertEqual(r.x, 4)
+        self.assertEqual(r.y, 6)
+        self.assertEqual(r.id, 2)
+
+
+    def test_to_dictionary_method(self):
+        """ Test the to_dictionary method """
+        r = Rectangle(5, 10, 2, 3, 1)
+        expected_dict = {'id': 1, 'width': 5, 'height': 10, 'x': 2, 'y': 3}
+        self.assertDictEqual(r.to_dictionary(), expected_dict)
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -13,7 +13,7 @@ class Test_Square(unittest.TestCase):
     def test_square(self):
         """ test square method """
         s1 = Square(5)
-        self.assertEqual(s1.id, 1)
+        self.assertEqual(s1.id, 7)
 
         s2 = Square(2, 2)
 
@@ -22,4 +22,63 @@ class Test_Square(unittest.TestCase):
         s3 = Square(3, 1, 3)
         self.assertEqual(s3.area(), 9)
 
-        #self.assertEqual(print(s3), '[Square] (3) 1/3 - 3')
+        """ self.assertEqual(print(s3), '[Square] (3) 1/3 - 3') """
+
+    def test_attributes(self):
+        """ Test attribute types and default values """
+        s = Square(5, 2, 3, 1)
+        self.assertIsInstance(s.size, int)
+        self.assertIsInstance(s.x, int)
+        self.assertIsInstance(s.y, int)
+        self.assertIsInstance(s.id, int)
+        self.assertEqual(s.size, 5)
+        self.assertEqual(s.x, 2)
+        self.assertEqual(s.y, 3)
+        self.assertIsNotNone(s.id)
+
+    def test_invalid_size(self):
+        """ Test invalid size value """
+        with self.assertRaises(ValueError):
+            Square(-5, 2, 3, 1)
+
+    def test_str_method(self):
+        """ Test the __str__ method """
+        s = Square(5, 2, 3, 1)
+        expected_output = "[Square] (1) 2/3 - 5"
+        self.assertEqual(str(s), expected_output)
+
+    def test_size_property(self):
+        """ Test the size property """
+        s = Square(5, 2, 3, 1)
+        self.assertEqual(s.size, 5)
+        s.size = 8
+        self.assertEqual(s.size, 8)
+
+    def test_update_method(self):
+        """ Test the update method """
+        s = Square(5, 2, 3, 1)
+        s.update(2, 8, 4, 6)
+        self.assertEqual(s.size, 8)
+        self.assertEqual(s.x, 4)
+        self.assertEqual(s.y, 6)
+        self.assertEqual(s.id, 2)
+
+    def test_to_dictionary_method(self):
+        """ Test the to_dictionary method """
+        s = Square(5, 2, 3, 1)
+        expected_dict = {'id': 1, 'size': 5, 'x': 2, 'y': 3}
+        self.assertDictEqual(s.to_dictionary(), expected_dict)
+
+    def test_update_method_invalid_arguments(self):
+        """ Test the update method with invalid arguments """
+        s = Square(5, 2, 3, 1)
+        with self.assertRaises(ValueError):
+            s.update(2, -8, 4, 6)
+
+    def test_update_method_keyword_arguments(self):
+        """ Test the update method with keyword arguments """
+        s = Square(5, 2, 3, 1)
+        s.update(size=8, x=4, y=6)
+        self.assertEqual(s.size, 8)
+        self.assertEqual(s.x, 4)
+        self.assertEqual(s.y, 6)
