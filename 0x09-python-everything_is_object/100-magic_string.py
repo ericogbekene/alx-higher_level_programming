32b91d20202c313608a9bccb082a5c71ccc95824
Eric Ogbekene <ericogbekene@icloud.com>
2024-01-02 12:06:45 -0800

    fix magic string requirements 4 lines

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    """ defines a magic string"""
     magic_string.idx = getattr(magic_string, 'idx', 0) + 1
     return ", ".join(["BestSchool" for _ in range(magic_string.idx)])
