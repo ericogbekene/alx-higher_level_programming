a32d50683c19c0e898582ee3d7bd511001a997ae
Eric Ogbekene <ericogbekene@icloud.com>
2024-01-18 09:36:50 -0800

    Create City with Foreign Key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- create a table with PRIMARY and FOREIGN Key
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT NOT NULL AUTO_GENERATED,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	UNIQUE (id),
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id) REFERENCES states(id)
+);
