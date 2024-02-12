c94470298bcdadeca08252d87219fc38d3775a01
Eric Ogbekene <ericogbekene@icloud.com>
2024-02-12 13:29:58 -0800

    convert first argument to integer

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 
-const numArgs = args.length;
-if (numArgs === 0) {
-  console.log('No argument');
-} else {
+if (args) {
   console.log(args[0]);
+} else {
+  console.log('No argument');
 }
