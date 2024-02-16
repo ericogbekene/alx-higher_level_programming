cde38cd59fb51a413a418afcac0bc13603a5b9a9
Eric Ogbekene <ericogbekene@icloud.com>
2024-02-16 11:32:05 -0800

    making it semi standard compliant

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 let count = 0;
 exports.nbOccurences = function (list, searchElement) {
-    for (let i = 0; i < list.length; i++){
-        if (list[i] === searchElement){
-            count += 1;
-        }
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count += 1;
     }
-    return count;
-}
+  }
+  return count;
+};
