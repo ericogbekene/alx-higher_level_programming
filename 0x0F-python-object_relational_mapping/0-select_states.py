dd58c300c007baaf20474ec8fb2073373e837a18
Eric Ogbekene <ericogbekene@icloud.com>
2024-02-17 16:42:16 -0800

    pycodestyle validation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,24 +4,23 @@
 if __name__ == '__main__':
     import MySQLdb
     import sys
-    
+
     args = sys.argv
 
-    """if (len(args) != 4):
+    if (len(args) != 4):
         print('Kindly enter at least 3 arguments')
-        """
 
     try:
-        db = MySQLdb.connect(host='localhost', port=3306, user=args[1], passwd=args[2], db=args[3])
+        db = MySQLdb.connect(host='localhost', port=3306, user=args[1],
+                             passwd=args[2], db=args[3])
 
         cur = db.cursor()
         cur.execute("SELECT * FROM states ORDER BY states.id ASC")
         rows = cur.fetchall()
         for row in rows:
-            print (row)
+            print(row)
         cur.close()
         db.close()
-            
+
     except Exception as e:
         print(e)
-        
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,20 +4,22 @@
 if __name__ == '__main__':
     import MySQLdb
     import sys
-    
+
     args = sys.argv
 
     if (len(args) != 4):
         print('Kindly enter at least 3 arguments')
 
     try:
-        db = MySQLdb.connect(host='localhost', port=3306, user=args[1], passwd=args[2], db=args[3])
+        db = MySQLdb.connect(host='localhost', port=3306, user=args[1],
+                             passwd=args[2], db=args[3])
 
         cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC")
+        cur.execute("SELECT * FROM states WHERE name \
+            LIKE BINARY 'N%' ORDER BY states.id ASC")
         rows = cur.fetchall()
         for row in rows:
-            print (row)
-            
+            print(row)
+
     except Exception as e:
         print(e)
