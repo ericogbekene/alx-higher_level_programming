f9aa74ea7d7d88f769f9c4e7bdec6fc955285f2c
Eric Ogbekene <ericogbekene@icloud.com>
2024-02-18 03:16:49 -0800

    second task to filter search

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Sql import using Mysqldb """
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import MySQLdb
     import sys
 
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 """
 a module to match a given argument
 with all records in a database
@@ -6,20 +6,25 @@ with all records in a database
 
 if __name__ == "__main__":
     import sys
+    import MySQLdb
+
     args = sys.argv
 
-    if len(args != 5):
+    if (len(args) != 5):
         raise Exception('Kindly enter required arguments')
     try:
         db = MySQLdb.connect(host='localhost', port=3306, user=args[1],
                              passwd=args[2], db=args[3])
 
         cur = db.cursor()
-        cur.execute("SET @matchName − = 'args[4]'; SELECT * FROM states WHERE name \
-             = @matchName ORDER BY states.id ASC")
+        my_query = "SELECT * FROM states WHERE name \
+             %s ORDER BY states.id ASC"
+        cur.execute(my_query, (args[4],))
         rows = cur.fetchall()
         for row in rows:
             print(row)
+        cur.close()
+        db.close()
 
     except Exception as e:
         print(e)
